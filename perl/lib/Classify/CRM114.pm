7d801db776077f8d183ec5baa4d15de797d10fcf
Martin Schuette <info@mschuette.name>
2012-05-09 20:54:59 +0000

    write doc, add verbatim flag to classify

--- perl/lib/Classify/CRM114.pm
+++ perl/lib/Classify/CRM114.pm
@@ -1,5 +1,34 @@
 package Classify::CRM114;
 
+=head1 NAME
+
+Classify::CRM114 - Perl interface for CRM114
+
+=head1 SYNOPSIS
+
+  use Classify::CRM114;
+  my $db = Classify::CRM114->new(
+    Classify::libcrm114::OSBF_BAYES,
+    8*1024*1024, ["Alice", "Macbeth"]);
+
+  $db->learn("Alice", "Alice was beginning to ...");
+  $db->learn("Macbeth", "When shall we three meet again ...");
+
+  my @ret = $db->classify_text("The Mole had been working very hard all the morning ...");
+
+  say "Best classification is $ret[1]" unless ($ret[0] != Classify::libcrm114::OK);
+
+=head1 DESCRIPTION
+
+This module provides a simple Perl interface to C<libcrm114>,
+a library that implements several text classification algorithms.
+
+=head1 METHODS
+
+=over
+
+=cut
+
 use 5.012000;
 use strict;
 use warnings;
@@ -13,21 +42,50 @@ our %EXPORT_TAGS = ( 'all' => [ qw(
 our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
 
 our @EXPORT = qw(
-	
+    
 );
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 my $debug = 0;
+
+=item Classify::CRM114->new($flags, $datasize, $classref)
+
+Creates a new instance.
+
+=over
+
+=item $flags
+
+sets the classification algorithm, recommended values are 
+
+C<Classify::libcrm114::OSB_BAYES> (default), 
+C<Classify::libcrm114::OSB_WINNOW>, or
+C<Classify::libcrm114::HYPERSPACE>.
+C<libcrm114> includes some more algorithms (SVM, PCA, FSCM) which
+may or may not be production ready.
+
+=item $datasize
+
+the memory size of learned data (default is 4 Mb).
+Note that some algorithms have to grow the datasize when learning.
+
+=item $classref
+
+a list of classes passed by reference (default: C<['A', 'B']>).
+
+=back
+
+=cut
 
 sub new {
     my ($class, $flags, $datasize, $classref) = @_;
     my $self = {};
     bless ($self, $class);
 
-	carp sprintf("%s->(0x%x, %s, %s)", $class,
-		$flags // 0, $datasize // "undef",
-		$classref // "undef") if ($debug);
+    carp sprintf("%s->(0x%x, %s, %s)", $class,
+        $flags // 0, $datasize // "undef",
+        $classref // "undef") if ($debug);
 
     # default values
     $flags    //= Classify::libcrm114::OSB;
@@ -50,6 +108,12 @@ sub new {
     return $self;
 }
 
+=item Classify::CRM114->readfile($filename)
+
+Creates a new instance by reading a previously saved CRM114 DB from C<$filename>.
+
+=cut
+
 sub readfile {
     my ($class, $filename) = @_;
     my $self = {};
@@ -71,9 +135,16 @@ sub readfile {
     return $self;
 }
 
+=item $db->getclasses()
+
+Returns a hash reference to the DB's classes.
+This hash associates the class names (keys) with the internal integer index (values).
+
+=cut
+
 sub getclasses {
-	my $self = shift;
-	return $self->{classmap};
+    my $self = shift;
+    return $self->{classmap};
 }
 
 sub DESTROY {
@@ -89,16 +160,28 @@ sub DESTROY {
     return;
 }
 
+=item $db->writefile($filename)
+
+Writes the DB into a (binary) file.
+
+=cut
+
 sub writefile {
     my ($self, $filename) = @_;
     carp "writefile($filename)" if ($debug);
     return Classify::libcrm114::db_write_bin($self->{db}, $filename);
 }
 
+=item $db->learn($class, $text)
+
+Learn some text of a given class.
+
+=cut
+
 sub learn {
     my ($self, $class, $text) = @_;
     croak("learn requires category and text as arguments")
-      unless (defined $class && defined $text);
+        unless (defined $class && defined $text);
 
     my $err = Classify::libcrm114::learn_text($self->{db}, $self->{classmap}->{$class}, $text, length($text));
     if ($err != Classify::libcrm114::OK) {
@@ -106,42 +189,108 @@ sub learn {
     }
 }
 
+=item $db->classify($text, $verbatim)
+
+Classify the text.
+
+The normal working mode without the optional C<$verbatim> flag adjusts the
+return values to be more useful with two classes (e.g. spam/ham).
+If the flag is given then the values are passed unchanged as they come
+from C<libcrm114>. In practice this is only relevant if you use more
+than two classes. (Then you have to consider the success/non-success classes
+and probably want to add a method to retrieve the single per-class results.)
+
+Returns a list of five scalar values:
+
+=over
+
+=item $err
+
+A numeric error code, should be C<Classify::libcrm114::OK>
+
+=item $errmsg
+
+A short error message (for error display or logging).
+
+=item $class
+
+The name of the best matching class.
+
+=item $prob
+
+The success probability. Normally the probability of the matching class (with 0.5 <= $prob <= 1)
+
+With C<$verbatim> this is the success probability, i.e. with two classes the
+probability of the first class and with multiple classes the sum of
+probabilities for all successful classes (with 0 <= $prob <= 1).
+
+=item $pR
+
+The logarithmic probability ratio i.e. C<log10($prob) - log10(1-$prob)>
+(theorethic range is 0 <= $pR <= 340, limited by floating point precision;
+but in practice a p = .99 yields a pR = 2, so high values are rather unusual).
+
+With C<$verbatim> this is the ratio between all success and all non-success
+probabilities, so for a non-successful result the value can also be negative
+(range -340 <= $pR <= 340).
+
+=back
+
+=cut
+
 sub classify {
-    my ($self, $text) = @_;
+    my ($self, $text, $verbatim) = @_;
     croak("classify_text requires a text as argument")
-      unless (defined $text);
+        unless (defined $text);
 
     my ($err, $class, $prob, $pR, $unk) = Classify::libcrm114::classify($self->{db}, $text, length($text));
     my $errmsg = Classify::libcrm114::strerror($err);
-    return ($err, $errmsg, $class, $prob, $pR, $unk);
+
+    if (!$verbatim and $self->{classmap}->{$class}) {
+        # change prob and pR values relative to second class
+        $prob = 1 - $prob;
+        $pR = -$pR;
+    }
+
+    return ($err, $errmsg, $class, $prob, $pR);
 }
 
-1;
-__END__
-# Below is stub documentation for your module. You'd better edit it!
+=back
 
-=head1 NAME
+=head1 ISSUES
 
-Classify::CRM114 - Perl interface for CRM114
+This is my first attempt to write a Perl module, so all hints and improvements
+are appreciated.
 
-=head1 SYNOPSIS
+I would like to hide the constants from C<Classify::libcrm114>.
+I guess it is impossible to eliminate the error codes (unless one wants
+to completely hide them from the user and simply C<croak> on every error).
+But at least for the algorithm selection I consider string arguments, i.e.
+the user should give us the string C<OSBF> and we map it to C<Classify::libcrm114::OSBF>.
 
-  use Classify::CRM114;
-  ...
+I wonder if we should ensure Classify::libcrm114::OK maps to 0, as this makes
+the caller's return value checking easier.
+Currently this is trivial because it already is 0 in C<libcrm114>.
+If that should change we would have to insert a rewrite into
+every XS call to a C function (ugly, but maybe worth it).
 
-=head1 DESCRIPTION
+I am still not sure if the C memory management works correctly.
 
-A simple Perl binding for functions in C<libcrm114>. You probably do not want to use
-this module directly. -- Classify::CRM114 instead, which hides some of the low level
-functions inside a Perl object.
-
-=head2 EXPORT
-
-None by default.
+Another issue is Unicode support, which is missing in C<libcrm114>, so it might
+be a good thing to convert unicode strings into some 8-bit encoding.
+As long as no string contains \0-values nothing bad[tm] will happen,
+but I assume that Unicode strings will internally cause wrong tokenization
+(this should be checked in C<libtre>).
 
 =head1 SEE ALSO
 
 CRM114 homepage: L<http://crm114.sourceforge.net/>
+
+AI::CRM114, a module using the crm language interpreter: L<https://metacpan.org/module/AI::CRM114>
+
+=head1 HISTORY
+
+v0.02 initial release
 
 =head1 AUTHOR
 
@@ -158,3 +307,6 @@ it under the terms of the GNU Lesser General Public License version 3.
 
 
 =cut
+
+1;
+__END__
--- perl/t/Classify-CRM114.t
+++ perl/t/Classify-CRM114.t
@@ -1,7 +1,7 @@
 use strict;
 use warnings;
 
-use Test::More tests => 36;
+use Test::More tests => 52;
 BEGIN { use_ok('Classify::CRM114') };
 use lib 't';
 BEGIN { use_ok('SampleText') };
@@ -20,9 +20,9 @@ my $filename = "/tmp/sample.db";
 my $rc = $db->writefile($filename);
 is($rc, Classify::libcrm114::OK);
 is(Classify::libcrm114::strerror($rc), "success");
-
 undef $db;
 
+# read anew from file 
 $db = Classify::CRM114->readfile($filename);
 
 my %classes = %{$db->getclasses()};
@@ -34,35 +34,61 @@ is($classes{"Macbeth"}, 1);
 is(scalar(keys(%classes)), 2);
 
 # result values from simple_demo.c
-my ($err, $errmsg, $class, $prob, $pR, $unk) = $db->classify(SampleText::Alice_frag());
+
+# "normal mode" -- adjusted values
+my($err, $errmsg, $class, $prob, $pR) = $db->classify(SampleText::Alice_frag());
 is($err, Classify::libcrm114::OK);
 is($errmsg, "success");
 is($class, "Alice");
 is(sprintf("%.1f", $prob), "1.0");
 is(sprintf("%.6f", $pR), "19.779991");
-is($unk, 220);
 
-($err, $errmsg, $class, $prob, $pR, $unk) = $db->classify(SampleText::Macbeth_frag());
+($err, $errmsg, $class, $prob, $pR) = $db->classify(SampleText::Macbeth_frag());
 is($err, Classify::libcrm114::OK);
 is($errmsg, "success");
 is($class, "Macbeth");
-is(sprintf("%.1f", $prob), "0.0");
-is(sprintf("%.6f", $pR), "-20.351904");
-is($unk, 236);
+is(sprintf("%.1f", $prob), "1.0");
+is(sprintf("%.6f", $pR), "20.351904");
 
-($err, $errmsg, $class, $prob, $pR, $unk) = $db->classify(SampleText::Hound_frag());
+($err, $errmsg, $class, $prob, $pR) = $db->classify(SampleText::Hound_frag());
 is($err, Classify::libcrm114::OK);
 is($errmsg, "success");
 is($class, "Macbeth");
-is(sprintf("%.3f", $prob), "0.404");
-is(sprintf("%.6f", $pR), "-0.169050");
-is($unk, 180);
+is(sprintf("%.3f", $prob), "0.596");
+is(sprintf("%.6f", $pR), "0.169050");
 
-($err, $errmsg, $class, $prob, $pR, $unk) = $db->classify(SampleText::Willows_frag());
+($err, $errmsg, $class, $prob, $pR) = $db->classify(SampleText::Willows_frag());
 is($err, Classify::libcrm114::OK);
 is($errmsg, "success");
 is($class, "Alice");
 is(sprintf("%.3f", $prob), "0.897");
 is(sprintf("%.6f", $pR), "0.938232");
-is($unk, 208);
 
+# "verbatim mode"
+($err, $errmsg, $class, $prob, $pR) = $db->classify(SampleText::Alice_frag(), 1);
+is($err, Classify::libcrm114::OK);
+is($errmsg, "success");
+is($class, "Alice");
+is(sprintf("%.1f", $prob), "1.0");
+is(sprintf("%.6f", $pR), "19.779991");
+
+($err, $errmsg, $class, $prob, $pR) = $db->classify(SampleText::Macbeth_frag(), 1);
+is($err, Classify::libcrm114::OK);
+is($errmsg, "success");
+is($class, "Macbeth");
+is(sprintf("%.1f", $prob), "0.0");
+is(sprintf("%.6f", $pR), "-20.351904");
+
+($err, $errmsg, $class, $prob, $pR) = $db->classify(SampleText::Hound_frag(), 1);
+is($err, Classify::libcrm114::OK);
+is($errmsg, "success");
+is($class, "Macbeth");
+is(sprintf("%.3f", $prob), "0.404");
+is(sprintf("%.6f", $pR), "-0.169050");
+
+($err, $errmsg, $class, $prob, $pR) = $db->classify(SampleText::Willows_frag(), 1);
+is($err, Classify::libcrm114::OK);
+is($errmsg, "success");
+is($class, "Alice");
+is(sprintf("%.3f", $prob), "0.897");
+is(sprintf("%.6f", $pR), "0.938232");
