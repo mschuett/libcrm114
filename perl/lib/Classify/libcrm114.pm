2e2fc451bb32484cf47c0497a84913e4eb029975
Martin Schuette <info@mschuette.name>
2012-05-18 14:30:19 +0200

    remove exports and autoloader

--- perl/lib/Classify/libcrm114.pm
+++ perl/lib/Classify/libcrm114.pm
@@ -5,97 +5,17 @@ use strict;
 use warnings;
 use Carp;
 
-require Exporter;
-use AutoLoader;
-
-our @ISA = qw(Exporter);
-
-# Items to export into callers namespace by default. Note: do not export
-# names by default without a very good reason. Use EXPORT_OK instead.
-# Do not simply export all your public functions/methods/constants.
-
-# This allows declaration	use Classify::libcrm114 ':all';
-# If you do not need this, moving things directly into @EXPORT or @EXPORT_OK
-# will save memory.
-our %EXPORT_TAGS = ( 'all' => [ qw(
-	cb_getdimensions
-	cb_read_text
-	cb_read_text_fp
-	cb_reset
-	cb_setblockdefaults
-	cb_setclassdefaults
-	cb_setdefaults
-	cb_setflags
-	cb_setregex
-	cb_write_text
-	cb_write_text_fp
-	classify_text
-	db_close_bin
-	db_read_bin
-	db_read_text
-	db_read_text_fp
-	db_write_bin
-	db_write_text
-	db_write_text_fp
-	free
-	learn_text
-	new_cb
-	new_db
-	show_result
-	show_result_class
-	strerror
-) ] );
-
-our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
-
-our @EXPORT = qw(
-	
-);
-
+# no exports
 our $VERSION = '0.02';
-
-sub AUTOLOAD {
-    # This AUTOLOAD is used to 'autoload' constants from the constant()
-    # XS function.
-
-    my $constname;
-    our $AUTOLOAD;
-    ($constname = $AUTOLOAD) =~ s/.*:://;
-    croak "&Classify::libcrm114::constant not defined" if $constname eq 'constant';
-    my ($error, $val) = constant($constname);
-    if ($error) { croak $error; }
-    {
-	no strict 'refs';
-	# Fixed between 5.005_53 and 5.005_61
-#XXX	if ($] >= 5.00561) {
-#XXX	    *$AUTOLOAD = sub () { $val };
-#XXX	}
-#XXX	else {
-	    *$AUTOLOAD = sub { $val };
-#XXX	}
-    }
-    goto &$AUTOLOAD;
-}
 
 require XSLoader;
 XSLoader::load('Classify::libcrm114', $VERSION);
 
-# Preloaded methods go here.
-
-# Autoload methods go after =cut, and are processed by the autosplit program.
-
 1;
 __END__
-# Below is stub documentation for your module. You'd better edit it!
-
 =head1 NAME
 
 Classify::libcrm114 - Perl interface for libcrm114
-
-=head1 SYNOPSIS
-
-  use Classify::libcrm114;
-  ...
 
 =head1 DESCRIPTION
 
